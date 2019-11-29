fdb6659246ea8896d096a724a1a7baaceeb1d492
Alex <alexkh403@gmail.com>
2019-11-29 20:22:18 +0500

    IGzipEngine

---
+++ GZipTest.Core/GZipEngine/IGzipEngine.cs
@@ -0,0 +1,13 @@
+﻿namespace GZipTest.Core.GZipEngine
+{
+    /// <summary>
+    /// Движок обработки файлов
+    /// </summary>
+    public interface IGzipEngine
+    {
+        /// <summary>
+        /// Запуск процедуры обработки файлов
+        /// </summary>
+        void Execute();
+    }
+}
